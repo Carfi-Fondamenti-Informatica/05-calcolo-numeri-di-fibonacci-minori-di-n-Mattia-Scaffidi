5b30b5681455ebf57260bbd02c9d5c749b87dd02
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-11-27 21:17:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+    int n, c;
+    int a = 1, b = 1;
+    cin >> n;
+
+
+    cout << a << "\n" << b << "\n";
+
+    for (int i = 2; i< n; i++) {
+
+        c = a + b;
+
+        a = b;
+        b = c;
+        if (c<=n) {
+            cout << c << endl;
+        }
+
+    }
    return 0;
 }
